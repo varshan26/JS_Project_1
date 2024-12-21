512047a5b45217f95a733f7a0d3dfc30a1226e3d
Varshan S <varshans@Varshans-Laptop.local>
2024-12-21 08:41:18 +0530

    added ui to js and html

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
 let humanScore = 0;
 let computerScore = 0;
-
+let humanchoice='';
+let computerchoice='';
+let final = '';
+let game_on = true;
 
 function getComputerChoice(){
     a = ["rock", "paper", "scissor"];
     return (a[Math.floor(Math.random()*3)]);
 };
 
-function getHumanChoice(){
-    let choice = prompt("pick your choice:")
-    return (choice.toLowerCase());
-};
 
 function playAround(humanchoice,computerchoice){
 
     console.log(humanchoice);
     console.log(computerchoice);
 
-    if(humanchoice == "rock" && computerchoice =="paper"){
-        console.log("You lose!");
-    }else if(humanchoice == "paper" && computerchoice =="scissor"){
-        console.log("You lose!");
-    }else if(humanchoice == "scissor" && computerchoice =="rock"){
-        console.log("You lose!");
-    }else if(humanchoice == computerchoice){
-        console.log("Its a Tie!")
-    }else{
-        console.log("you Win!")
+    if(game_on){
+        if(humanchoice == "rock" && computerchoice =="paper"){
+            console.log("You lose!");
+            computerScore+=1
+        }else if(humanchoice == "paper" && computerchoice =="scissor"){
+            console.log("You lose!");
+            computerScore+=1
+        }else if(humanchoice == "scissor" && computerchoice =="rock"){
+            console.log("You lose!");
+            computerScore+=1
+        }else if(humanchoice == computerchoice){
+            console.log("Its a Tie!")
+        }else{
+            console.log("you Win!")
+            humanScore+=1
+        }
+
+        if(humanScore==5){
+            console.log("You are the final Winner!");
+            game_on=false;
+            const container_3 = document.querySelector("#finale")
+            container_3.textContent=("You are the final Winner!")
+        }else if(computerScore==5){
+            console.log("The computer is the winner!")
+            game_on=false;
+            const container_3 = document.querySelector("#finale")
+            container_3.textContent=("You Loser!")
+        }
+
+        const container = document.querySelector("#hscore");
+        container.textContent=("Your Score: "+humanScore.toString());
+        
+        const container_2 = document.querySelector("#cscore");
+        container_2.textContent=("Computer Score: "+computerScore.toString());
+
+        console.log("HumanScore :",humanScore);
+        console.log("ComputerScore :",computerScore);  
+        
     }
+
+    
 }
 
-playAround(getHumanChoice(),getComputerChoice());
+const btn_1 = document.querySelector("#rock")
+btn_1.addEventListener("click",()=>{
+    playAround("rock",getComputerChoice());
+})
+
+const btn_2 = document.querySelector("#paper")
+btn_2.addEventListener("click",()=>{
+    playAround("paper",getComputerChoice())
+})
+
+const btn_3 = document.querySelector("#scissor")
+btn_3.addEventListener("click",()=>{
+    playAround("scissor",getComputerChoice())
+})
+
+
+
